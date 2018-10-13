854319094664aa09806a36d659a0dc52f61deba0
Colton Lipchak <clipchak@coltons-macbook-air-2.local>
2018-10-13 17:45:58 -0400

    Simple flashcards lab done

--- Flashcard/ViewController.swift
+++ Flashcard/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var QuestionLabel: UILabel!
+    @IBOutlet weak var AnswerLabel: UILabel!
+    
+    @IBOutlet weak var cardContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        QuestionLabel.layer.cornerRadius = 20.0
+        AnswerLabel.layer.cornerRadius = 20.0
+        
+        QuestionLabel.clipsToBounds = true
+        AnswerLabel.clipsToBounds = true
+       
+        
+        
     }
 
-
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        if(QuestionLabel.isHidden){
+            QuestionLabel.isHidden = false;
+        }
+        else{
+            QuestionLabel.isHidden = true
+    }
+    }
+    
 }
 
